294054041eb0fc59425d794d41d937b2d9930385
IgorS <amsterdam1611@gmail.com>
2015-12-21 20:08:38 +0400

    Gulp Signed-off-by: IgorS <amsterdam1611@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
+"use strict";
+
 var gulp = require("gulp"),
+    wiredep = require('wiredep').stream,
     browserSync = require('browser-sync');
+
+// сборка html css javascript + удаление папки dist
+var rimraf = require('gulp-rimraf'),    
+    useref = require('gulp-useref'),    
+    uglify = require('gulp-uglify'),
+    gulpif = require('gulp-if'), 
+    minifyCss = require('gulp-minify-css');
 
 gulp.task('server', function () {
     browserSync ({
@@ -16,6 +26,29 @@ gulp.task('watch', function () {
         'app/js/**/*.js',
         'app/css/**/*.css'
     ]).on('change', browserSync.reload);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
 gulp.task('default', ['server', 'watch']);
+
+// Следим за bower
+    gulp.task('wiredep', function () {
+      gulp.src('app/*.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('app/'))
+    });
+
+// Переносим HTML, CSS, JS в папку dist 
+    gulp.task('useref', function () {
+      return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss({compatibility: 'ie8'})))
+        .pipe(gulp.dest('dist'));
+    });
+
+// Очистка
+    gulp.task('clean', function() {
+        return gulp.src('dist', { read: false }) 
+        .pipe(rimraf());
+    });
