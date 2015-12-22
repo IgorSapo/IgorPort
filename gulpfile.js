f243e491fab9d852658780be309862654a3903a8
IgorS <amsterdam1611@gmail.com>
2015-12-22 23:12:24 +0400

    Мелкие правки Signed-off-by: IgorS <amsterdam1611@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,38 @@ var rimraf = require('gulp-rimraf'),
     gulpif = require('gulp-if'), 
     minifyCss = require('gulp-minify-css');
 
+// финальная сборка
+var filter = require('gulp-filter'), 
+        imagemin = require('gulp-imagemin'),
+        size = require('gulp-size'); 
+
+
+// Перенос шрифтов
+        gulp.task('fonts', function() {
+          gulp.src('app/fonts/*')
+            .pipe(filter(['*.eot','*.svg','*.ttf','*.woff','*.woff2']))
+            .pipe(gulp.dest('dist/fonts/'))
+        });
+
+// Картинки
+        gulp.task('images', function() {
+          return gulp.src('app/img/**/*')
+            .pipe(imagemin({
+              progressive: true,
+              interlaced: true
+            }))
+            .pipe(gulp.dest('dist/img'));
+        });
+
+// Остальные файлы, такие как favicon.ico и пр.
+        gulp.task('extras', function () {
+          return gulp.src([
+            'app/*.*',
+            '!app/*.html'
+          ]).pipe(gulp.dest('dist'));
+        });
+
+// Загружаем сервер
 gulp.task('server', function () {
     browserSync ({
         port: 9000,
@@ -20,6 +52,7 @@ gulp.task('server', function () {
     });
 });
 
+// Слежка
 gulp.task('watch', function () {
     gulp.watch([
         'app/*.html',
@@ -52,3 +85,38 @@ gulp.task('default', ['server', 'watch']);
         return gulp.src('dist', { read: false }) 
         .pipe(rimraf());
     });
+
+    // Сборка и вывод размера содержимого папки dist
+gulp.task('dist', ['useref', 'images', 'fonts', 'extras'], function () {
+  return gulp.src('dist/**/*').pipe(size({title: 'build'}));
+});
+
+// Собираем папку DIST (только после компиляции Jade)
+gulp.task('build', ['clean'], function () {
+  gulp.start('dist');
+});
+
+// ====================================================
+// ====================================================
+// ===================== Функции ======================
+
+// Более наглядный вывод ошибок
+var log = function (error) {
+  console.log([
+    '',
+    "----------ERROR MESSAGE START----------",
+    ("[" + error.name + " in " + error.plugin + "]"),
+    error.message,
+    "----------ERROR MESSAGE END----------",
+    ''
+  ].join('\n'));
+  this.end();
+}
+
+
+// ====================================================
+// ====================================================
+// =============== Важные моменты  ====================
+// gulp.task(name, deps, fn) 
+// deps - массив задач, которые будут выполнены ДО запуска задачи name
+// внимательно следите за порядком выполнения задач!
